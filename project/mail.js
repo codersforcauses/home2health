a4ede5451b53a08e7c7a51730904a63311348571
shazzatherazza <52185121+shazzatherazza@users.noreply.github.com>
2019-07-08 19:40:10 +1000

    Added environment variables
    Added environment variables to store email credentials.

--- project/mail.js
+++ project/mail.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: process.env.NODEMAILER_SERVICE,
     secure: false,
     port: 25,
     auth: {
-        // What email address are we sending messages from and to?
-        user: "###USERNAME###",
-        pass: "###PASSWORD###"
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS
     },
     tls: {
         rejectUnauthorized: false
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
 const sendMail = (name, email, message, cb) => {
     const mailOptions = {
         from: name + "<" + email + ">",
-        to: "###RESEARCHER'S EMAIL###",
+        to: process.env.NODEMAILER_TO,
         subject: name + " sent a message via Home2Health",
         text: message
     }
