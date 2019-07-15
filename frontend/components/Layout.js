998ebb9c64fb5d159ebf2a628d9ca10cf970ef28
jkkim <j.k.kim98@outlook.com>
2019-07-15 16:52:36 +0800

    stick the footer

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Header from './Header'
 import Head from './Meta'
 import '../public/stylesheets/style.css'
@@ -7,12 +7,20 @@ import Footer from './Footer'
 class Layout extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <Head />
-        <Header />
-        {this.props.children}
-        <Footer />
-      </div>
+        <div
+          style={{
+            display: 'flex',
+            minHeight: '100vh',
+            flexDirection: 'column'
+          }}
+        >
+          <Header />
+          <div style={{ flex: '1 0 auto' }}>{this.props.children}</div>
+          <Footer />
+        </div>
+      </Fragment>
     )
   }
 }
