546cf7bc33dd7097fe04183ec5c3a1fef625b579
jkkim <j.k.kim98@outlook.com>
2019-07-16 14:34:03 +0800

    round borders only on top

--- frontend/components/ProfileContent.js
+++ frontend/components/ProfileContent.js
@@ -27,18 +27,25 @@ class ProfileContent extends Component {
     return (
       <Fragment>
         <div class="row grey lighten-1" style={{ borderRadius: '1rem' }}>
-          <div class="col s12">
-            <ul class="tabs">
+          <div class="col s12" style={{padding: 0}}>
+            <ul class="tabs" style={{ borderTopLeftRadius: '1rem', borderTopRightRadius: '1rem' }}>
               <li class="tab col s4 grey lighten-1">
-                <a onClick={(e) => this.handleTabChange('toggleAbout', e)}>Profile</a>
+                <a href="#about" onClick={(e) => this.handleTabChange('toggleAbout', e)}>Profile</a>
               </li>
               <li class="tab col s4 grey lighten-1">
-                <a onClick={(e) => this.handleTabChange('togglePosts', e)}>Posts</a>
+                <a href="#posts" onClick={(e) => this.handleTabChange('togglePosts', e)}>Posts</a>
               </li>
               <li class="tab col s4 grey lighten-1">
-                <a onClick={(e) => this.handleTabChange('toggleComments', e)}>Comments</a>
+                <a href="#comments" onClick={(e) => this.handleTabChange('toggleComments', e)}>Comments</a>
               </li>
             </ul>
+            <div id="about" class="blue">
+                <ProfileAbout />
+            </div>
+            <div id="posts" class="green">
+            </div>
+            <div id="comments" class="red">
+            </div>
           </div>
         </div>
       </Fragment>
