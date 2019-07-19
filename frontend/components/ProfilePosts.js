a8403ab4d3376bd6394bcd5700e049b5cde7c190
jkkim <j.k.kim98@outlook.com>
2019-07-19 13:53:14 +0800

    format

--- frontend/components/ProfilePosts.js
+++ frontend/components/ProfilePosts.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react'
 
 const PostItem = props => {
   return (
-    <div class="card grey lighten-5" style={{boxShadow: '0 0 0 0'}}>
+    <div class="card grey lighten-5" style={{ boxShadow: '0 0 0 0' }}>
       <div class="card-content">
         <div class="card-title">
-          <a href="#">
-            {props.title}
-          </a>
-          </div>
-          <p class="truncate">{props.body}</p>
+          <a href="#">{props.title}</a>
+        </div>
+        <p class="truncate">{props.body}</p>
       </div>
     </div>
   )
@@ -32,9 +30,18 @@ class ProfilePosts extends Component {
   render() {
     return (
       <div class="container" style={{ width: '90%' }}>
-        {(this.state.posts.length) ? this.state.posts.map(post => (
-          <PostItem title={post.title} body={post.body} />
-        )) : <h5 class="center-align" style={{marginTop: "5em", marginBottom: "5em"}}>It's so cold and lonely here...</h5>}
+        {this.state.posts.length ? (
+          this.state.posts.map(post => (
+            <PostItem title={post.title} body={post.body} />
+          ))
+        ) : (
+          <h5
+            class="center-align"
+            style={{ marginTop: '5em', marginBottom: '5em' }}
+          >
+            It's so cold and lonely here...
+          </h5>
+        )}
       </div>
     )
   }
