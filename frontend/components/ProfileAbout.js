fcb99670534ba2d1aa8e177408696e35ef5e6d9a
jkkim <j.k.kim98@outlook.com>
2019-07-21 23:26:11 +0800

    remove weeb

--- frontend/components/ProfileAbout.js
+++ frontend/components/ProfileAbout.js
@@ -4,16 +4,8 @@ class ProfileAbout extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        aboutme: 'Kurisu Makise is the deuteragonist and \
-        lead heroine of the visual novel and anime series \
-        Steins;Gate, as well as one of the main protagonists \
-        of the sequel Steins;Gate 0. She is a young and \
-        extremely talented neuroscience researcher.\nHer route \
-        is the route that leads to the "true" and complete \
-        ending of the game.',
-        qualifications: 'On September 25th 2014, Kurisu Makise \
-        won the AnimeBracket - Best Girl Contest, with 6620 votes. \
-        She topped the Aquamarine 1 stage of the International Saimoe contest'
+      aboutme: 'Please describe yourself',
+      qualifications: 'Please add qualifications'
     }
   }
 
@@ -21,13 +13,17 @@ class ProfileAbout extends Component {
     return (
       <div class="container" style={{ width: '90%' }}>
         <div class="section">
-          <h4 style={{margin: '1rem 0 0 0'}}>About Me</h4>
-          <p>{this.state.aboutme}</p>
+          <h4 style={{ margin: '1rem 0 0 0' }}>About Me</h4>
+          <div class="container" style={{ width: '90%' }}>
+            <p>{this.state.aboutme}</p>
+          </div>
         </div>
         <div class="divider" />
         <div class="section">
-          <h4 style={{margin: '1rem 0 0 0'}}>Qualifications</h4>
-          <p>{this.state.qualifications}</p>
+          <h4 style={{ margin: '1rem 0 0 0' }}>Qualifications</h4>
+          <div class="container" style={{ width: '90%' }}>
+            <p>{this.state.qualifications}</p>
+          </div>
         </div>
       </div>
     )
--- frontend/pages/user/[id].js
+++ frontend/pages/user/[id].js
@@ -7,18 +7,18 @@ class Index extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      id: '004',
-      name: 'Kurisu Makise',
-      title: 'Genius Girl, The Zombie, Assistant, Christina, Celeb Thirty',
-      img: 'https://png2.kisspng.com/sh/0d78ce487ddfee85052c70224a50b262/L0KzQYm3V8ExN6tAf5H0aYP2gLBuTft2epp4jZ92YXvsg7a0kvlvfJJ3hAc2b3vkcra0kCRmcZ94RdlqdHWwQH70ggl2epoyiAZuaX72PbjolPUuc6Z3geVALUXlRIWAgcI3a5MAftYDLka6RoO3V8E3OWY4SaQ6NEO6RIm6VMEveJ9s/kisspng-kurisu-makise-rintarou-okabe-steins-gate-0-mayuri-steins-gate-kurisu-5b447a26cb9fd8.6762071615312143748341.png',
-      profession: 'Lab Member 004',
-      workplace: 'Future Gadget Lab',
-      address: 'Akihabara, Taito City, Tokyo 110-0006, Japan',
+      id: '1',
+      name: 'John Doe',
+      title: 'Professor, BSc Calcutta, MSc Kanpur, MTech PhD IIT Madras',
+      img: '',
+      profession: 'Lecturer',
+      workplace: 'UWA',
+      address: 'Perth, Western Australia',
       phone: '123456789',
-      email: 'celebthirty@gmail.com',
-      website: 'https://steins-gate.fandom.com/wiki/Kurisu_Makise',
-      facebook: 'https://steins-gate.fandom.com/wiki/Kurisu_Makise',
-      twitter: 'https://steins-gate.fandom.com/wiki/Kurisu_Makise',
+      email: 'johndoe@email.com',
+      website: 'https://www.johndoe.com',
+      facebook: 'https://www.facebook.com',
+      twitter: 'https://www.twitter.com',
     }
   }
   render() {
