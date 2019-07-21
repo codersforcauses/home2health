bd44f174129c5b0f1601b310fb064fddf5cc7ef0
jkkim <j.k.kim98@outlook.com>
2019-07-21 23:54:31 +0800

    connect to local db

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,23 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const sendMail = require('./mail')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const emailRouter = require('./routes/email')
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log(`connected to database at ${process.env.MONGO_URI}`)
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
