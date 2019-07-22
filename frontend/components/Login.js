fecd7450f37e56a70eda26ddbf786ca659aa7467
jkkim <j.k.kim98@outlook.com>
2019-07-22 14:46:51 +0800

    remove use of react-bootstrap and add login page

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,69 +1,60 @@
 /*jshint esversion: 6 */
 
-import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
-import FormGroup from "react-bootstrap/FormGroup";
-import FormControl from "react-bootstrap/FormControl";
-import ControlLabel from "react-bootstrap/ControlLabel";
-import "./Login.css";
+import React, { Component } from 'react'
+import './Login.css'
 
 export default class Login extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      email: "",
-        password: ""
-    };
+      email: '',
+      password: ''
+    }
   }
 
   validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0; // ensures that the correct input is in the sectino, modify to add autho0 verification
+    return this.state.email.length > 0 && this.state.password.length > 0 // ensures that the correct input is in the sectino, modify to add autho0 verification
   }
 
   handleChange = event => {
     this.setState({
-      [event.target.id] : event.target.value
-    });
+      [event.target.id]: event.target.value
+    })
   }
 
   handleSubmit = event => {
-      event.preventDefault();
+    event.preventDefault()
   }
 
   render() {
-      return (
-          <div className = "Login">
-              <form onSubmit={this.handleSubmit}>
-                  <FormGroup controlID = "email" bsSize = "large">
-                      <ControlLabel>Email</ControlLabel>
-                      <FormControl
-                        autoFocus
-                        type = "email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
-                    </FormGroup>
-                    <FormGroup controlID = "password" bsSize = "large">
-                        <ControlLabel>Password</ControlLabel>
-                        <FormControl
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type = "password"
-                    />
-                    </FormGroup>
-                <Button
-                    block
-                    beSize = "large"
-                    disabled={!this.validateForm}
-                    type = "submit"
-                >
-                    Login
-                </Button>
-            </form>
-        </div>
-      );
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <input
+            autoFocus
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+          <input
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password"
+          />
+          <div
+            class="btn"
+            block
+            beSize="large"
+            disabled={!this.validateForm}
+            type="submit"
+          >
+            Login
+          </div>
+        </form>
+      </div>
+    )
   }
 }
-
-      
