c5dc6488409fe0d4cd4c318ab59d6984bca263c2
Shayan Rasaratnam <shayan@bloom.org.au>
2019-08-22 11:10:32 +0800

    Updated contact form to work with React

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const sendMail = require('../mail.js')
 const router = express.Router()
 
 /* GET users listing. */
 router.post('/', (req, res) => {
   const { name, organisation, email, message } = req.body
+  console.log(name, organisation, email, message)
   sendMail(name, organisation, email, message, function(err, data) {
     if (err) {
       res.json({
