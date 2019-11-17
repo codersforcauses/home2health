ec8b2d65b5c0373d56bea3df9e600f443fcfc8df
Innocent Muisha <innocent.m@outlook.com>
2019-11-17 20:29:59 +0800

    Starting auth0 Integration
    Created the files and added in the essential code to fully use auth0...

--- backend/app.js
+++ backend/app.js
@@ -44,3 +44,69 @@ app.use(function(err, req, res, next) {
 })
 
 module.exports = app
+
+// auth0 configuration 
+
+var session = require('express-session');
+
+// config express-session
+var sess = {
+  secret: 'pY1Mv4ERgy8MF5Fp5727yJwIchMydEwpPkJiAwyWVXG88bHMyzszYXKSAgde1gXK',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
+
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
+
+app.use(session(sess));
+
+// auth0 passport configuration (authentication middleware)
+
+// Load environment variables from .env
+var dotenv = require('dotenv');
+dotenv.config();
+
+// Load Passport
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var userInViews = require('./lib/middleware/userInViews');
+var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/* uses the secured middle ware to secure the route */
+router.get('/', secured(), function(req, res, next) {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render('user', {
+    userProfile: JSON.stringify(userProfile, null, 2),
+    title: 'Profile page'
+  });
   res.send('respond with a resource');
 });
 
