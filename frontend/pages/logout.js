430cc22727cf86ead91cfe7bf6a463c58c4bd2fc
shazzatherazza <52185121+shazzatherazza@users.noreply.github.com>
2020-02-10 11:54:25 +1100

    Updated logout.js
    Fixed the next/router issue that @frinzekt mentioned

--- frontend/pages/logout.js
+++ frontend/pages/logout.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Consumer } from '../Context'
+import AppContext, { Consumer } from '../Context'
 import Router from 'next/router'
-export default LogOut => {
-  return (
-    <Consumer>
-      {context => {
-        context.actions
-          .signOut()
-          .then(Router.replace('/'))
-          .catch(err => console.log(err))
-      }}
-    </Consumer>
-  )
+
+export default class Logout extends React.Component {
+  static contextType = AppContext
+  componentDidMount() {
+    const context = this.context
+    const { pathname } = Router
+    if (!context.authenticatedUser && typeof window !== 'undefined') {
+      context.actions.redirectToSignIn(pathname)
+    } else {
+      context.actions
+        .signOut()
+        .then(Router.replace('/'))
+        .catch(err => console.log(err))
+    }
+  }
+  render() {
+    return <div></div>
+  }
 }
