d19212720da049ec207f36a4f75609bd7070d4e5
frinzekt <frinzekt@gmail.com>
2020-02-15 12:28:21 +0800

    refactoring

--- frontend/pages/housingprograms/index.js
+++ frontend/pages/housingprograms/index.js
@@ -7,6 +7,72 @@ export default () => {
     marginRight: 0,
     marginTop: 50
   }
+
+  const Card = ({ alt, src, link, reportTitle, date }) => (
+    <div className="col m4 s6">
+      <div className="card">
+        <div className="card-image">
+          <img alt={alt} src={src} width="200px" height="162px" />
+        </div>
+        <div className="card-action">
+          <a href={link}>
+            {reportTitle}
+            <br />
+            {date ? `(${date})` : ''}
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+  const Cards = () => {
+    const cardDetails = [
+      {
+        alt: '50 Lifes 50 Homes logo',
+        src: '/50Homes50Lifes/logo2-50L.png',
+        link: '/50Homes50Lifes/50_Lives_First_Report_June_2017.pdf',
+        reportTitle: '50 Lives 50 Homes Report 1 (June 2017)'
+      },
+      {
+        alt: '50 Lifes 50 Homes logo',
+        src: '/50Homes50Lifes/InfoGraphics50H.png',
+        link: '/50Homes50Lifes/50_Lives_Infographic_June_2017.pdf',
+        reportTitle: '50 Lives 50 Homes Infographic (June 2017)'
+      },
+      {
+        alt: '50 Lifes 50 Homes logo',
+        src: '/50Homes50Lifes/50HomesDoc.png',
+        link: '/50Homes50Lifes/50_Lives_Snapshot_April_2018.pdf',
+        reportTitle: '50 Lives 50 Homes Snapshot Report ',
+        date: 'Apr 2018'
+      },
+      {
+        alt: '50 Lifes 50 Homes logo',
+        src: '/50Homes50Lifes/logo2-50L.png',
+        link: '/50Homes50Lifes/50_Lives_Second_Report_Sept_2018.pdf',
+        reportTitle: '50 Lives 50 Homes Report 2 (Sept 2018)'
+      },
+      {
+        alt: '50 Lifes 50 Homes logo',
+        src: '/images/landing/9.jpg',
+        link: 'Hospital Collaboration Paper (Wood et al 2018)',
+        reportTitle: 'Hospital Collaboration Paper (Wood et al 2018)'
+      },
+      {
+        alt: '50 Lifes 50 Homes logo',
+        src: '/50Homes50Lifes/fb_img.png',
+        link: 'https://www.facebook.com/50Lives50HomesPerth/',
+        reportTitle: 'Our Facebook Page'
+      }
+    ]
+
+    return (
+      <div class="row">
+        {cardDetails.map((card, i) => (
+          <Card {...card} key={i}></Card>
+        ))}
+      </div>
+    )
+  }
   return (
     <div className="container">
       <img
@@ -20,7 +86,7 @@ export default () => {
         src="/50Homes50Lifes/50life-logo.png"
       />
       <h4 style={{ marginBottom: 0 }}>About 50 Lifes 50 Homes</h4>
-      <h6>
+      <p>
         The 50 Lives 50 Homes project is the first Housing First program in
         Western Australia, and one of the first in Australia. 50 Lives is a
         collective impact initiative aiming to provide long-term housing and
@@ -45,126 +111,19 @@ export default () => {
         <br />
         <br />
         See below for more information:
-      </h6>
+      </p>
       <div style={{ textAlign: 'center' }}>
-        <div className="row">
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  alt="50 Lifes 50 Homes logo"
-                  src="/50Homes50Lifes/logo2-50L.png"
-                  width="200px"
-                  height="162px"
-                />
-              </div>
-              <div className="card-action">
-                <a href="/50Homes50Lifes/50_Lives_First_Report_June_2017.pdf">
-                  50 Lives 50 Homes Report 1 (June 2017)
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  alt="50 Lifes 50 Homes logo"
-                  src="/50Homes50Lifes/InfoGraphics50H.png"
-                  width="200px"
-                  height="162px"
-                />
-              </div>
-              <div className="card-action">
-                <a href="/50Homes50Lifes/50_Lives_Infographic_June_2017.pdf">
-                  Infographic <br />
-                  (June 2017)
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  alt="50 Lifes 50 Homes logo"
-                  src="/50Homes50Lifes/50HomesDoc.png"
-                  width="200px"
-                  height="162px"
-                />
-              </div>
-              <div className="card-action">
-                <a href="/50Homes50Lifes/50_Lives_Snapshot_April_2018.pdf">
-                  Snapshot Report <br />
-                  (Apr 2018)
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  alt="50 Lifes 50 Homes logo"
-                  src="/50Homes50Lifes/logo2-50H.png"
-                  width="200px"
-                  height="162px"
-                />
-              </div>
-              <div className="card-action">
-                <a href="/50Homes50Lifes/50_Lives_Second_Report_Sept_2018.pdf">
-                  50 Lives 50 Homes Report 2 (Sept 2018)
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  alt="50 Lifes 50 Homes logo"
-                  src="/images/landing/9.jpg"
-                  width="200px"
-                  height="162px"
-                />
-              </div>
-              <div className="card-action">
-                <a href="/50Homes50Lifes/Wood_et_al_2018_-_Hospital_Collaboration_with_a_Housing_First_program.pdf">
-                  Hospital Collaboration Paper (Wood et al 2018)
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="col s4">
-            <div className="card">
-              <div className="card-image">
-                <img
-                  alt="50 Lifes 50 Homes logo"
-                  src="/50Homes50Lifes/fb_img.png"
-                  width="200px"
-                  height="162px"
-                />
-              </div>
-              <div className="card-action">
-                <a href="https://www.facebook.com/50Lives50HomesPerth/">
-                  Our Facebook Page
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Cards></Cards>
       </div>
       <h4 style={{ marginBottom: 0 }}>Early Initiative: 20 Lifes 20 Homes</h4>
-      <h6 style={{ margin: '1rem 0 0' }}>
+      <p style={{ margin: '1rem 0 0' }}>
         The 20 Lives 20 Homes project will be the first place-based trial of the
         50 Homes 50 Lives housing first model in the metropolitan area, based in
         Fremantle. The St Patrick’s 20 Lives 20 Homes team will work along with
         Ruah and a range of collaborating agencies, to provide intensive wrap
         around support to individuals facing complex barriers to exiting
         homelessness. This project is due to commence in 2019.
-      </h6>
+      </p>
     </div>
   )
 }
