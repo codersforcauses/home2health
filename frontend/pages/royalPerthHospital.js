2c06d608dc60ab97dd0843fa8d3b893cf2a6a9c3
Innocent Muisha <innocent.m@outlook.com>
2020-02-22 14:58:17 +0800

    Pre-master merge into pages

--- frontend/pages/royalPerthHospital.js
+++ frontend/pages/royalPerthHospital.js
@@ -27,23 +27,43 @@ class RoyalPerthHospital extends Component {
             </h4>
             <p className="flow-text"> </p>
           </div>
-          <div className="carousel" size="large">
-            <a className="carousel-item" href="#one!">
-              <img src="https://lorempixel.com/250/250/nature/1" />
-            </a>
-            <a className="carousel-item" href="#two!">
-              <img src="https://lorempixel.com/250/250/nature/2" />
-            </a>
-            <a className="carousel-item" href="#three!">
-              <img src="https://lorempixel.com/250/250/nature/3" />
-            </a>
-            <a className="carousel-item" href="#four!">
-              <img src="https://lorempixel.com/250/250/nature/4" />
-            </a>
-            <a className="carousel-item" href="#five!">
-              <img src="https://lorempixel.com/250/250/nature/5" />
-            </a>
+          <div>
+            <blockquote>
+              *Royal Perth Hospital is a major public teaching hospital located
+              in the Perth CBD. In mid-2016, Homeless Healthcare established its
+              first in-reach program in collaboration with Royal Perth Hospital.
+              The in reach program is modelled on the internationally renowned
+              and evidence based Pathway program that operates in 11 hospitals
+              in the UK. The aim of RPH Homeless Team is to improve outcomes for
+              homeless patients by providing support, improved discharge
+              planning and continuity of care. GPs and nurses from Homeless
+              Healthcare work alongside RPH Homeless Team staff.
+            </blockquote>
           </div>
+          <div>
+            <img
+              src="../public/images/hospitalPrograms/royalPerth/003_Homeless-Healthcare_160914.jpg"
+              alt="RPH Work"
+            />
+          </div>
+
+          {/* <div className="carousel" size="large">
+          <a className="carousel-item" href="#one!">
+            <img src="https://lorempixel.com/250/250/nature/1" />
+          </a>
+          <a className="carousel-item" href="#two!">
+            <img src="https://lorempixel.com/250/250/nature/2" />
+          </a>
+          <a className="carousel-item" href="#three!">
+            <img src="https://lorempixel.com/250/250/nature/3" />
+          </a>
+          <a className="carousel-item" href="#four!">
+            <img src="https://lorempixel.com/250/250/nature/4" />
+          </a>
+          <a className="carousel-item" href="#five!">
+            <img src="https://lorempixel.com/250/250/nature/5" />
+          </a>
+        </div> */}
         </div>
       </Fragment>
 
