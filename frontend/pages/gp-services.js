2f1d8617c6dfef4bf3a36251e4aa24ee94f13eb7
Frank <franksgro@outlook.com>
2020-02-22 10:41:50 +0800

    Auto stash before merge of "GP_Services_Page" and "origin/master"

--- frontend/pages/gp-services.js
+++ frontend/pages/gp-services.js
@@ -1,23 +1,46 @@
+import { LinkCards } from '../components/LinkCards'
+
 const GpServices = () => {
   return (
     <>
       {style}
-      <h1>GP Services</h1>
-      <h3>Homeless Healthcare</h3>
-      Homeless Healthcare is a primary health service for people experiencing,
-      or at risk of, homelessness. It offers a variety of services to help
-      people experiencing homelessness in Perth including: 
-      • Street health:
-      Nurses providing care in public places within the Perth CBD and Fremantle
-      to target rough sleepers’ medical needs • Mobile GP: Clinics are provided
-      at homeless drop-in centres throughout the Perth CBD 
-      • After-hours support
-      services (AHSS): 24-hour support program provided to 50 Lives 50 Homes
-      clients 
-      • RPH in-reach: Supports patients and connects them with community
-      services. 
-      • Dual diagnosis service (HODDS) The Home2Health research team
-      has been conducting an ongoing evaluation of these services since 2016.
+      <div className="container">
+        {/* <h1>GP Services</h1> */}
+        <h3>Homeless Healthcare</h3>
+
+        <p>
+          Homeless Healthcare is a primary health service for people
+          experiencing, or at risk of, homelessness. It offers a variety of
+          services to help people experiencing homelessness in Perth including:
+          <ul>
+            <li>
+              • Street health: Nurses providing care in public places within the
+              Perth CBD and Fremantle to target rough sleepers’ medical needs
+            </li>
+            <li>
+              • Mobile GP: Clinics are provided at homeless drop-in centres
+              throughout the Perth CBD
+            </li>
+            <li>
+              • After-hours support services (AHSS): 24-hour support program
+              provided to 50 Lives 50 Homes clients
+            </li>
+            <li>
+              • RPH in-reach: Supports patients and connects them with community
+              services.
+            </li>
+            <li>
+              • Dual diagnosis service (HODDS) The Home2Health research team has
+              been conducting an ongoing evaluation of these services since
+              2016.
+            </li>
+          </ul>
+        </p>
+        <p>
+          The Home2Health research team has been conducting an ongoing
+          evaluation of these services since 2016.
+        </p>
+      </div>
     </>
   )
 }
