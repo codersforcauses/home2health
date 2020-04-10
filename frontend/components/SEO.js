7ad5c8d4b7c7da71077f1be8b04f8a824c662a22
frinzekt <frinzekt@gmail.com>
2020-04-11 00:28:27 +0800

    seo changes

--- frontend/components/SEO.js
+++ frontend/components/SEO.js
@@ -5,39 +5,49 @@ import Helmet from 'react-helmet'
 const SEO = ({
   metaDescription = 'Home2Health',
   lang = 'en',
-  meta,
   keywords = 'Home2Health',
-  title = 'Home2Health'
+  meta,
+  title = 'Home2Health',
+  url = 'https://home2health.org.au',
+  image = '/images/landing/1.jpg',
 }) => {
   return (
     <Helmet
       htmlAttributes={{
-        lang
+        lang,
       }}
       title={title}
       meta={[
         {
           name: `description`,
-          content: metaDescription
+          content: metaDescription,
         },
         {
           property: `og:title`,
-          content: title
+          content: title,
         },
         {
           property: `og:description`,
-          content: metaDescription
+          content: metaDescription,
         },
         {
           property: `og:type`,
-          content: `website`
-        }
+          content: `website`,
+        },
+        {
+          property: `og:url`,
+          content: url,
+        },
+        {
+          property: `og:image`,
+          content: image,
+        },
       ]
         .concat(
           keywords.length > 0
             ? {
                 name: `keywords`,
-                content: keywords.join(`, `)
+                content: keywords.join(`, `),
               }
             : []
         )
@@ -49,7 +59,7 @@ const SEO = ({
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
-  keywords: []
+  keywords: [],
 }
 
 SEO.propTypes = {
@@ -57,7 +67,7 @@ SEO.propTypes = {
   lang: PropTypes.string,
   meta: PropTypes.array,
   keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string
+  title: PropTypes.string,
 }
 
 export default SEO
