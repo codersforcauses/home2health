2665c921a8d778627c4c302cabea82a28480f592
Drewbi <drewalexander986@gmail.com>
2020-04-17 19:01:40 +0800

    changed slider functionality

--- frontend/components/Slider.js
+++ frontend/components/Slider.js
@@ -4,22 +4,28 @@ import withAutoplay from 'react-awesome-slider/dist/autoplay'
 import 'react-awesome-slider/dist/styles.css'
 import './Slider.css'
 
+const images = [
+  "images/landing/1.jpg",
+  "images/landing/2.jpg",
+  "images/landing/3.jpg",
+  "images/landing/4.jpg",
+  "images/landing/5.jpg",
+  "images/landing/6.jpg",
+  "images/landing/7.jpg",
+  "images/landing/8.jpg",
+  "images/landing/9.jpg",
+  "images/landing/10.jpg"
+]
+
+const imageTags = images.map((image, index) => (<div data-src={image} key={index} />))
+
 const Slider = props => {
   const AutoplaySlider = withAutoplay(AwesomeSlider)
   return (
     <div className="slider">
       <div className="slider-wrapper">
         <AutoplaySlider play={true} cancelOnInteraction={false} interval={2000}>
-          <div data-src="images/landing/1.jpg" />
-          <div data-src="images/landing/2.jpg" />
-          <div data-src="images/landing/3.jpg" />
-          <div data-src="images/landing/4.jpg" />
-          <div data-src="images/landing/5.jpg" />
-          <div data-src="images/landing/6.jpg" />
-          <div data-src="images/landing/7.jpg" />
-          <div data-src="images/landing/8.jpg" />
-          <div data-src="images/landing/9.jpg" />
-          <div data-src="images/landing/10.jpg" />
+          {imageTags}
         </AutoplaySlider>
       </div>
     </div>
